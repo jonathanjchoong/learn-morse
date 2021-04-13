d5262861060594ad2409ad972cd5d971bedc5ae3
Vecko <nickhodgskin@gmail.com>
2021-04-13 12:37:40 +0800

    WIP Implementation of audio. Currently is not asyncronous leading to tying up of the JavaScript environment. Need to implement callback functions etc.

--- Client/JavaScript/translator.js
+++ Client/JavaScript/translator.js
@@ -1,6 +1,15 @@
 // This script is to implement some useful functions and objects that will be used accross
 // all learning pages.
 
+/// TEMPORARY FUNCTION UNTIL WE IMPLEMENT ASYNCRONOUS MORSE CODE PLAYING (SO IT DOESN'T PAUSE THE WEBPAGE)
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
 const translator = {}
 // Defining objects to look up letters and morse.
 translator.letter_to_morse = // Source https://gist.github.com/mohayonao/094c71af14fe4791c5dd
@@ -70,12 +79,60 @@ translator.func.word_to_morse = function (word) {//Takes a word and returns an a
     return output;
 }
 
-translator.func.morse_to_word = function (morse) {//Takes an array of morse and returns a word
+translator.func.morse_to_word = function (morse) {
+    //Takes an array of morse and returns a word
+    //It will ignore unrecognised characters
     output = ""
     for (i = 0; i < morse.length; i++) {
         morse_letter = translator.morse_to_letter[morse[i]]
-        if (morse_letter === undefined){return undefined}
+        if (morse_letter === undefined){continue;}
         output += morse_letter
     }
     return output;
 }
+
+translator.func.sentence_to_morse = function(sentence){
+    return 'test';
+}
+
+const player = {};
+player.timings = {};
+player.dot = new Audio("../Assets/523.3Hz_0.2s.wav");
+player.dash = new Audio("../Assets/523.3Hz_0.6s.wav");
+player.timings.inter_blip = 0.2 * 1000
+player.timings.inter_letter = 0.6 * 1000;
+player.timings.inter_word = 1.4 * 1000;
+
+player.play_morse = function(morse_string){
+    // Takes in a string (or array) of dots, dashes, spaces, and slashes and plays them.
+    // Spaces indicate gaps between letters, slashes indicate gaps between words
+    for (i=0; i<morse_string.length; i++){
+        play_character = morse_string[i];
+        switch (play_character){
+            case ".":
+                player.dot.play();
+                sleep(player.timings.inter_blip);
+            case "-":
+                player.dash.play();
+                sleep(player.timings.inter_blip);
+            case " ":
+                sleep(player.timings.inter_letter);
+            case "/":
+                sleep(player.timings.inter_word)
+            default:
+                continue
+        }
+
+    }
+
+}
+
+// player.play = function(sentence){
+//     sentence = sentence.toLowerCase();
+//     for (i=0; i<sentence.length; i++){
+//         played_character = sentence[i];
+//         if played_
+//     }
+
+// }
+
