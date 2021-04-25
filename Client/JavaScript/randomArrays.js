f843aeb195a7e3f34bd52465852a6f5dd0399f40
Dean Law <deanbrandon.law@gmail.com>
2021-04-25 10:49:25 +0800

    added button functionality for long and short press and reset button

---
+++ Client/JavaScript/randomArrays.js
@@ -0,0 +1,85 @@
+//random
+//math.random is a random between 0 - 1
+//length of array * math.random, floored
+const dict = {};
+dict.letter_to_morse = // Source https://gist.github.com/mohayonao/094c71af14fe4791c5dd
+{
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--.."
+};
+
+// arrays of morse and letters 
+var letterArray = [];
+var morseArray = [];
+
+for (let tuple of Object.entries(dict.letter_to_morse)){
+    letterArray.push(tuple[0]);
+    morseArray.push(tuple[1])
+};
+
+//randomise letters
+function randomLetterIndex(){
+    return math.floor(math.random() * letterArray.length)
+};
+
+//randomise morse
+function randomMorseIndex(){
+    return math.floor(math.random() * morseArray.length)
+};
+
+//get an array of three 
+var testArray = []
+function arrayOfThree(type = 'Letter'){
+    // New array populate with three letters right off the bat
+    if (textArray.length==0){
+        for (let i=0; i < 3 ; i++ ){
+            if (type = 'Letter'){
+                let numIndex = randomLetterIndex();
+                let letter = letterArray[numIndex];
+                testArray.push(letter)
+            } else {
+                let numIndex = randomMorseIndex();
+                let morse = morseArray[numIndex];
+                testArray.push(morse)
+            }
+        }
+    } else { //when there is already populated drop the first index and then repopulate
+        textArray.shift()
+        if (type = 'Letter'){
+            let numIndex = randomLetterIndex();
+            let letter = letterArray[numIndex];
+            testArray.push(letter)
+        } else {
+            let numIndex = randomMorseIndex();
+            let morse = morseArray[numIndex];
+            testArray.push(morse)
+        }
+    }
+    return testArray
+}
+
+
