fed830858a73ef9c67f97daa374caab991fde2d0
Vecko <nickhodgskin@gmail.com>
2021-05-09 12:34:40 +0800

    Implemented User and Play models including back-referencing and password hashing. Implemented tests for backreffing and password hashing. All OK.

--- app/models.py
+++ app/models.py
@@ -1,24 +1,47 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
+from app import db
 import os
 
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key = True)
+    # Defining columns
+    id = db.Column(db.Integer, primary_key = True)
     display_name = db.Column(db.String(20), nullable = False)
     email = db.Column(db.String(320), unique=True, nullable = False)
-    hashed_password = db.Column(db.String(128), nullable = False)
+    password_hash = db.Column(db.String(128), nullable = False)
 
-    # TODO: Relationships and backrefs with plays and tokens
+    # Defining relationships
+    plays = db.relationship("Play", backref = "user", lazy = True) # Enables user.plays to return the users games. Backref allows Plays.user to return a user object
+    tokens = db.relationship("Token", backref = "user", lazy = True)
 
-class Plays(db.Model):
-    play_id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer)
-    letter_guessed = db.Column(db.String(1))
-    is_correct = db.Column(db.Boolean)
-    game_mode = db.Column(db.Integer) # options are 1 or 2
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        return
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-    # TODO: Relationships/backrefs with user
+    def __repr__(self):
+        return f"<User user_id={self.id}, display_name={self.display_name}>"
 
-class Tokens(db.Model):
-    pass
-    # TODO: Relationships/backrefs with user
+
+class Play(db.Model):
+    # Defining columns
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
+    letter_guessed = db.Column(db.String(1), nullable = False)
+    is_correct = db.Column(db.Boolean, nullable = False)
+    game_mode = db.Column(db.Integer, nullable = False) # options are 1 or 2
+
+    def __repr__(self):
+        return f"<Play {self.id}>"
+
+class Token(db.Model):
+    # Defining columns
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
+    token_string = db.Column(db.String(128), nullable = False)
+    
+    def __repr__(self):
+        return f"<Play {self.id}>"
+
+    # TODO: Implement token generation (ie. a function set_token())
